150ef82555771c3acbc3f66c74e916b71d72a911
123sswss <1147381311@qq.com>
2026-07-25 00:48:31 +0800

    3

--- run_experiment.sh
+++ run_experiment.sh
@@ -203,34 +203,15 @@ run_fixed_seed_sequence() {
   done
 }
 
-# 两张卡使用同一新 seed 做配对结构实验。
+# 常用实验入口。
 #   bash run_experiment.sh relation44
 #   bash run_experiment.sh relation47
-#   bash run_experiment.sh random_init47
-#   bash run_experiment.sh random_init44
-#   bash run_experiment.sh hybrid_init47
-#   bash run_experiment.sh zero_init47
-#   bash run_experiment.sh zero_init44
-#   bash run_experiment.sh zero_init48
-#   bash run_experiment.sh router_zero44
-#   bash run_experiment.sh router_zero48
-#   bash run_experiment.sh one_epoch_lr6
-#   bash run_experiment.sh one_epoch_lr4
 #   bash run_experiment.sh heterogeneous_lr_pair
-#   bash run_experiment.sh heterogeneous_lr_up44
-#   bash run_experiment.sh heterogeneous_lr_up48
-#   bash run_experiment.sh heterogeneous_lr_wide44
-#   bash run_experiment.sh heterogeneous_lr_down44
 #   bash run_experiment.sh heterogeneous_relation_100_102
 #   bash run_experiment.sh multiturn_relation_100_102
-#   bash run_experiment.sh multiturn_step940_seed100
-#   bash run_experiment.sh first_turn_seed100
-#   bash run_experiment.sh joint_multiturn_seed100
-#   bash run_experiment.sh joint_module_lr_seed100
 #   bash run_experiment.sh joint_cosine_seed100
+#   bash run_experiment.sh joint_cosine_44_46
 #   bash run_experiment.sh heterogeneous_no_relation_100_102
-#   bash run_experiment.sh guard44
-#   bash run_experiment.sh guard47
 #   bash run_experiment.sh raw_adapter47
 # 当前激进消融默认固定 seed 44：
 #   bash run_experiment.sh direct_mmrl
@@ -247,82 +228,12 @@ case "$RUN_TARGET" in
     FIXED_SEED=47
     run_one "visual_router_relation_alignment_diag" "visual_router_relation_retest_seed47"
     ;;
-  random_init47)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=47
-    run_one "visual_router_relation_random_adapter_init_v1" "visual_router_relation_random_adapter_init_v1_seed47"
-    ;;
-  random_init44)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=44
-    run_one "visual_router_relation_random_adapter_init_v1" "visual_router_relation_random_adapter_init_v1_seed44"
-    ;;
-  hybrid_init47)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=47
-    run_one "visual_router_relation_hybrid_adapter_init_v1" "visual_router_relation_hybrid_adapter_init_v1_seed47"
-    ;;
-  zero_init47)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=47
-    run_one "visual_router_relation_true_zero_adapter_init_v1" "visual_router_relation_true_zero_adapter_init_v1_seed47"
-    ;;
-  zero_init44)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=44
-    run_one "visual_router_relation_true_zero_adapter_init_v1" "visual_router_relation_true_zero_adapter_init_v1_seed44"
-    ;;
-  zero_init48)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=48
-    run_one "visual_router_relation_true_zero_adapter_init_v1" "visual_router_relation_true_zero_adapter_init_v1_seed48"
-    ;;
-  router_zero44)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=44
-    run_one "visual_router_relation_true_zero_adapter_router_init_v1" "visual_router_relation_true_zero_adapter_router_init_v1_seed44"
-    ;;
-  router_zero48)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=48
-    run_one "visual_router_relation_true_zero_adapter_router_init_v1" "visual_router_relation_true_zero_adapter_router_init_v1_seed48"
-    ;;
-  one_epoch_lr6)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=44
-    run_one "visual_router_relation_one_epoch_lr6e5_v1" "visual_router_relation_one_epoch_lr6e5_v1_seed44"
-    ;;
-  one_epoch_lr4)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=44
-    run_one "visual_router_relation_one_epoch_lr4e5_v1" "visual_router_relation_one_epoch_lr4e5_v1_seed44"
-    ;;
   heterogeneous_lr_pair)
     AUTO_INCREMENT_SEED=0
     FIXED_SEED=44
     run_one "visual_router_relation_heterogeneous_adapter_lr_v1" "visual_router_relation_heterogeneous_adapter_lr_v1_seed44"
     FIXED_SEED=48
     run_one "visual_router_relation_heterogeneous_adapter_lr_v1" "visual_router_relation_heterogeneous_adapter_lr_v1_seed48"
-    ;;
-  heterogeneous_lr_up44)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=44
-    run_one "visual_router_relation_heterogeneous_adapter_lr_up_v2" "visual_router_relation_heterogeneous_adapter_lr_up_v2_seed44"
-    ;;
-  heterogeneous_lr_up48)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=48
-    run_one "visual_router_relation_heterogeneous_adapter_lr_up_v2" "visual_router_relation_heterogeneous_adapter_lr_up_v2_seed48"
-    ;;
-  heterogeneous_lr_wide44)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=44
-    run_one "visual_router_relation_heterogeneous_adapter_lr_wide_v3" "visual_router_relation_heterogeneous_adapter_lr_wide_v3_seed44"
-    ;;
-  heterogeneous_lr_down44)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=44
-    run_one "visual_router_relation_heterogeneous_adapter_lr_down_v3" "visual_router_relation_heterogeneous_adapter_lr_down_v3_seed44"
     ;;
   heterogeneous_relation_100_102)
     run_fixed_seed_sequence \
@@ -350,34 +261,6 @@ case "$RUN_TARGET" in
       "visual_router_relation_multiturn_fixed_v1" \
       100 101 102
     ;;
-  multiturn_step940_seed100)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=100
-    run_one \
-      "visual_router_relation_multiturn_step940_v1" \
-      "visual_router_relation_multiturn_step940_v1_seed100"
-    ;;
-  first_turn_seed100)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=100
-    run_one \
-      "visual_router_relation_first_turn_v1" \
-      "visual_router_relation_first_turn_v1_seed100"
-    ;;
-  joint_multiturn_seed100)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=100
-    run_one \
-      "visual_router_relation_joint_multiturn_seed42_v1" \
-      "visual_router_relation_joint_multiturn_seed42_v1_seed100"
-    ;;
-  joint_module_lr_seed100)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=100
-    run_one \
-      "visual_router_relation_joint_multiturn_module_lr_v2" \
-      "visual_router_relation_joint_multiturn_module_lr_v2_seed100"
-    ;;
   joint_cosine_seed100)
     AUTO_INCREMENT_SEED=0
     FIXED_SEED=100
@@ -385,21 +268,17 @@ case "$RUN_TARGET" in
       "visual_router_relation_joint_multiturn_cosine_v2" \
       "visual_router_relation_joint_multiturn_cosine_v2_seed100"
     ;;
+  joint_cosine_44_46)
+    run_fixed_seed_sequence \
+      "visual_router_relation_joint_multiturn_cosine_v2" \
+      "visual_router_relation_joint_multiturn_cosine_clean_1_14" \
+      44 45 46
+    ;;
   heterogeneous_no_relation_100_102)
     run_fixed_seed_sequence \
       "visual_router_no_relation_heterogeneous_adapter_lr_v1" \
       "visual_router_no_relation_heterogeneous_adapter_lr_v1_paired" \
       100 101 102
-    ;;
-  guard44)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=44
-    run_one "visual_router_relation_early_guard_v1" "visual_router_relation_early_guard_v1_seed44"
-    ;;
-  guard47)
-    AUTO_INCREMENT_SEED=0
-    FIXED_SEED=47
-    run_one "visual_router_relation_early_guard_v1" "visual_router_relation_early_guard_v1_seed47"
     ;;
   raw_adapter47)
     AUTO_INCREMENT_SEED=0
@@ -422,7 +301,7 @@ case "$RUN_TARGET" in
     run_one "visual_router_raw_adapter_v1" "visual_router_raw_adapter_v1_seed47"
     ;;
   *)
-    echo "[ERR] 未知实验目标: $RUN_TARGET（可选: relation44, relation47, random_init47, random_init44, hybrid_init47, zero_init47, zero_init44, zero_init48, router_zero44, router_zero48, one_epoch_lr6, one_epoch_lr4, heterogeneous_lr_pair, heterogeneous_lr_up44, heterogeneous_lr_up48, heterogeneous_lr_wide44, heterogeneous_lr_down44, heterogeneous_relation_100_102, multiturn_relation_seed100, multiturn_relation_seed101, multiturn_relation_100_102, multiturn_step940_seed100, first_turn_seed100, joint_multiturn_seed100, joint_module_lr_seed100, joint_cosine_seed100, heterogeneous_no_relation_100_102, guard44, guard47, raw_adapter47, direct_mmrl, two_adapter, single_adapter, all）" >&2
+    echo "[ERR] 未知实验目标: $RUN_TARGET（可选: relation44, relation47, heterogeneous_lr_pair, heterogeneous_relation_100_102, multiturn_relation_seed100, multiturn_relation_seed101, multiturn_relation_100_102, joint_cosine_seed100, joint_cosine_44_46, heterogeneous_no_relation_100_102, raw_adapter47, direct_mmrl, two_adapter, single_adapter, all）" >&2
     exit 2
     ;;
 esac
